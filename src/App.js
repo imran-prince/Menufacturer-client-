16a5d7bdc3990f62d6616c02a4cde26e8ae69149
imran-hossain <csebubt35@gmail.com>
2022-05-22 07:45:11 +0600

    daisy ui setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+ 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-       hello bike mama
+    <div  >
+    <button class="btn btn-primary">Button</button>
     </div>
   );
 }
