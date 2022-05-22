b57bfdcc060513e60c08f0f66dc0e163735d91dc
imran-hossain <csebubt35@gmail.com>
2022-05-22 08:13:53 +0600

    custom Active link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
  
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Home from './Components/Home';
+import Navbar from './Shared/Navbar';
 
 function App() {
   return (
     <div  >
-    <button class="btn btn-primary">Button</button>
+     <Navbar></Navbar>
+     <Routes>
+       <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/home' element={<Home></Home>}></Route>
+       <Route path='/about' element={<About></About>}></Route>
+       <Route path='/contact' element={<Contact></Contact>}></Route>
+     </Routes>
     </div>
   );
 }
