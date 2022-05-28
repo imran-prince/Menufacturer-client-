990b7a0cd1e6e96c12376c18afb63967f1bf30eb
imran-hossain <csebubt35@gmail.com>
2022-05-28 17:35:35 +0600

    bux fix

--- src/Components/OrderPayment.js
+++ src/Components/OrderPayment.js
@@ -1,12 +1,11 @@
 import React from 'react';
  
-import useAllProduct from './hooks/useAllProduct';
+ 
 
 const OrderPayment = () => {
  
-    const [products] = useAllProduct()
-    const orderProduct = products.find(product => product._id === _id)
-    console.log(products)
+ 
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col  ">
@@ -15,7 +14,7 @@ const OrderPayment = () => {
 
                     <div className="card w-96 bg-base-100 shadow-xl">
                         <figure className="px-10 pt-10">
-                            <img src={orderProduct?.img} alt="Engin" className="rounded-xl" />
+                            
                         </figure>
                         <div className="card-body items-center text-center">
                             Payment System....Upcoming......
